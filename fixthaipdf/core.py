8e20d253e0803989f4bc46fb45c08a2ca179177c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2023-09-29 16:17:48 +0700

    Update more dict

--- fixthaipdf/core.py
+++ fixthaipdf/core.py
@@ -20,6 +20,7 @@ from pythainlp.corpus import thai_words
 from pythainlp.tokenize import Tokenizer,Trie
 import re
 from fixthaipdf.dict import replace_list
+from fixthaipdf.more_dict import replace_list_after_clean
 
 replace_list+=[(i.replace("า","ำ"),i) for i in list(thai_words()) if "า" in i]
 replace_list+=[(i.replace("ำ","า"),i) for i in list(thai_words()) if "ำ" in i]
@@ -39,6 +40,12 @@ trie_word = Trie(word_list)
 tokenizer=Tokenizer(trie_word)
 thai_characters_without_dig = ''.join([i for i in thai_characters if i not in thai_digits])
 
+
+replace_list_after_clean_key = set([i for i,_ in replace_list_after_clean])
+d2={i:j for i,j in replace_list_after_clean}
+word_list_2 = list(thai_words())+[i for i,_ in replace_list_after_clean]
+trie_word_2 = Trie(word_list_2)
+tokenizer2=Tokenizer(trie_word_2,engine="mm")
 
 def _clean_missing_1(text):
     text = text.replace("\uf70a","่") # ไม้เอก
@@ -70,5 +77,17 @@ def _clean_missing_1(text):
     text = text.replace(" ์","์")
     return normalize(text)
 
+
+def _clean_after_1(text):
+    _text=[]
+    for w in tokenizer2.word_tokenize(text):
+        if w in replace_list_after_clean_key:
+            _text.append(d2[w])
+        else:
+            _text.append(w)
+    return ''.join(_text)
+
 def clean(text: str)->str:
-    return _clean_missing_1(text)
+    text = _clean_missing_1(text)
+    text = _clean_after_1(text)
+    return text
